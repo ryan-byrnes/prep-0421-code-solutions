a3534d7e5fa11633aa0a8b3ab72ce5cddb4f42ce
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-04-28 17:38:27 -0700

    Completed prep-javascript-arrays.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,33 @@
+var languagesArray = ["html", "css", "javascript", "react", "node.js"];
+
+console.log(languagesArray);
+
+console.log(languagesArray.length);
+
+languagesArray.push("c#", "php");
+
+console.log(languagesArray);
+
+languagesArray.pop();
+
+console.log(languagesArray);
+
+languagesArray.unshift("c++");
+
+console.log(languagesArray);
+
+languagesArray.shift();
+
+console.log(languagesArray);
+
+var thirdElement = languagesArray[2];
+
+console.log(thirdElement);
+
+var arrayLength = languagesArray.length;
+
+console.log(arrayLength);
+
+var lastItem = languagesArray[languagesArray.length - 1];
+
+console.log(lastItem);
